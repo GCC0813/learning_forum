9f6803387cc44512fa0accfb34d5c6c6d8cd88e5
GCC <gechenchen@gongchangtemai.com.cn>
2021-05-13 11:07:12 +0800

    fix

--- db/learning_forum.sql
+++ db/learning_forum.sql
@@ -1,110 +1,61 @@
-/*
- Navicat Premium Data Transfer
+CREATE DATABASE learning_forum;
 
- Source Server         : Mysql5.7
- Source Server Type    : MySQL
- Source Server Version : 50728
- Source Host           : 127.0.0.1:3306
- Source Schema         : learning_forum
-
- Target Server Type    : MySQL
- Target Server Version : 50728
- File Encoding         : 65001
-
- Date: 04/04/2021 20:57:09
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for answer
--- ----------------------------
-DROP TABLE IF EXISTS `answer`;
-CREATE TABLE `answer`  (
+-- User表
+CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `code` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `content` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `created_date` datetime(0) NULL DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `introduction` varchar(16) DEFAULT NULL,
+  `password` varchar(60) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8
+
+-- topic表
+CREATE TABLE `topic` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category` varchar(16) NOT NULL,
+  `code` varchar(1024) DEFAULT NULL,
+  `content` varchar(1024) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `title` varchar(32) NOT NULL,
+  `id_user` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK6br5a7f1rn1bi94rkmi38mcln` (`id_user`),
+  CONSTRAINT `FK6br5a7f1rn1bi94rkmi38mcln` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8
+
+-- answer表
+CREATE TABLE `answer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(1024) DEFAULT NULL,
+  `content` varchar(1024) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
   `useful` bit(1) NOT NULL,
-  `id_topic` bigint(20) NULL DEFAULT NULL,
-  `id_user` bigint(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `FKjyya1ipmyfw7iea1kr80c43h1`(`id_topic`) USING BTREE,
-  INDEX `FK8ddy56hrr3tsqi29o3i5q48al`(`id_user`) USING BTREE,
-  CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `answer_ibfk_2` FOREIGN KEY (`id_topic`) REFERENCES `topic` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 141 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `id_topic` bigint(20) DEFAULT NULL,
+  `id_user` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKjyya1ipmyfw7iea1kr80c43h1` (`id_topic`),
+  KEY `FK8ddy56hrr3tsqi29o3i5q48al` (`id_user`),
+  CONSTRAINT `FK8ddy56hrr3tsqi29o3i5q48al` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKjyya1ipmyfw7iea1kr80c43h1` FOREIGN KEY (`id_topic`) REFERENCES `topic` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=140 DEFAULT CHARSET=utf8
 
--- ----------------------------
--- Records of answer
--- ----------------------------
-INSERT INTO `answer` VALUES (140, '1', 'Mybatis有什么好处？', '2021-04-04 20:49:17', b'0', 69, 24);
-
--- ----------------------------
--- Table structure for image
--- ----------------------------
-DROP TABLE IF EXISTS `image`;
-CREATE TABLE `image`  (
+-- message表
+CREATE TABLE `message` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `imgUrl` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `id_user` bigint(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 51 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `from_id` bigint(20) DEFAULT NULL,
+  `to_id` bigint(20) DEFAULT NULL,
+  `content` text,
+  `created_date` datetime DEFAULT NULL,
+  `id_topic` bigint(20) DEFAULT NULL,
+  `has_read` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8
 
--- ----------------------------
--- Table structure for message
--- ----------------------------
-DROP TABLE IF EXISTS `message`;
-CREATE TABLE `message`  (
+-- image表
+CREATE TABLE `image` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `from_id` bigint(20) NULL DEFAULT NULL,
-  `to_id` bigint(20) NULL DEFAULT NULL,
-  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `created_date` datetime(0) NULL DEFAULT NULL,
-  `id_topic` bigint(20) NULL DEFAULT NULL,
-  `has_read` int(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 81 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for topic
--- ----------------------------
-DROP TABLE IF EXISTS `topic`;
-CREATE TABLE `topic`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `category` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `code` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `content` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `created_date` datetime(0) NULL DEFAULT NULL,
-  `title` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `id_user` bigint(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `FK6br5a7f1rn1bi94rkmi38mcln`(`id_user`) USING BTREE,
-  CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 70 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of topic
--- ----------------------------
-INSERT INTO `topic` VALUES (69, 'mbs', '123', 'MyBatis 本是apache的一个开源项目iBatis, 2010年这个项目由apache software foundation 迁移到了google code，并且改名为MyBatis 。2013年11月迁移到Github。\r\niBATIS一词来源于“internet”和“abatis”的组合，是一个基于Java的持久层框架。iBATIS提供的持久层框架包括SQL Maps和Data Access Objects（DAOs）', '2021-04-04 20:48:48', 'Mybatis简介', 24);
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `created_date` datetime(0) NULL DEFAULT NULL,
-  `introduction` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `password` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `username` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (24, '2021-04-04 20:47:45', '哈哈哈', '$2a$10$3364rI16UBOjJLbDQZ/xhuRdBOaSgHUD5WCiMrLSpBN1QRtoRgbdC', 'liuzhimin');
-
-SET FOREIGN_KEY_CHECKS = 1;
+  `imgUrl` varchar(100) NOT NULL,
+  `id_user` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8
